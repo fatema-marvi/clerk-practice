76f7bb8db880aa496672929605f3b0cd70d773e5
fatema-marvi <160221766+fatema-marvi@users.noreply.github.com>
2024-12-12 17:33:09 +0500

    Update _app.tsx

--- app/_app.tsx
+++ app/_app.tsx
@@ -3,7 +3,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const clerkPubKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+  const clerkPubKey ="=pk_test_dW5pZmllZC1rb2ktNDUuY2xlcmsuYWNjb3VudHMuZGV2JA";
 
   if (!clerkPubKey) {
     throw new Error('Clerk publishable key is missing. Please set NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY in your environment variables.');
