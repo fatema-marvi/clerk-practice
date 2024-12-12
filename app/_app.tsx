9c8f3163e678cdda036cfa61a2d532f8b48624bb
fatema-marvi <160221766+fatema-marvi@users.noreply.github.com>
2024-12-12 17:47:56 +0500

    Update _app.tsx

--- app/_app.tsx
+++ app/_app.tsx
@@ -3,7 +3,10 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const clerkPubKey ="=pk_test_dW5pZmllZC1rb2ktNDUuY2xlcmsuYWNjb3VudHMuZGV2JA";
+  const clerkPubKey =process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || "fallback-key"
+
+  if (!clerkPubKey || clerkPubKey === "fallback-key") {
+    console.warn("Using fallback key. Check your environment variables.");}
 
   if (!clerkPubKey) {
     throw new Error('Clerk publishable key is missing. Please set NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY in your environment variables.');
